20308d3e98035075b2652d1e14c53dfae04a7f3f
vika <37137412+hi-im-vika@users.noreply.github.com>
2025-03-17 10:56:27 -0700

    Add more detail to issue

--- src/CZoomyClient.cpp
+++ src/CZoomyClient.cpp
@@ -350,7 +350,7 @@ void CZoomyClient::draw() {
         }
     }
 
-    // memory leak somewhere after this point inside draw() loop
+    // memory leak somewhere after this point inside draw() loop, only on macos
     // if viewport is minimized, don't draw
     if (SDL_GetWindowFlags(_window->get_native_window()) & SDL_WINDOW_MINIMIZED) return;
 
